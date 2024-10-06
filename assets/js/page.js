8e526e0e84b0f1070dcd9bb83257a982666b5ee0
Buse Savaş <81926506+buseesavas@users.noreply.github.com>
2024-10-07 01:48:49 +0300

    Add files via upload

---
+++ assets/js/page.js
@@ -0,0 +1,4 @@
+let saatBasiUcret = prompt('Saat başına aldığın ücret kaç TL?');
+let saat = prompt('Bu ay toplam kaç saat çalıştın');
+let toplamKazanc = saat * saatBasiUcret;
+alert('Toplam kazancın: ' + toplamKazanc + 'TL');
